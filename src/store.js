f8bbe3867f07d557c0bf6f96e1af3a5f63b31870
DeepLoyingFola <160628781+DeepLoyingFola@users.noreply.github.com>
2024-12-09 06:48:30 -0800

    commit for redux set up, dice bet api, and ui modification in progress v2

--- src/store.js
+++ src/store.js
@@ -3,12 +3,18 @@ import diceSummaryReducer from "../src/pages/admin/api_detaills/GlobalStates/dic
 import NigerianUsersReducer from "./pages/admin/api_detaills/GlobalStates/NigerianUsersSlice";
 import OnlineUsersReducers from "./pages/admin/api_detaills/GlobalStates/onlineUsers";
 import DiceBetsListReducer from "./pages/admin/api_detaills/GlobalStates/DiceBetsList";
+import LoosingDiceBetsListReducer from "./pages/admin/api_detaills/GlobalStates/LoosingDiceBetsListSlice";
+import WinningDiceBetsListReducer from "./pages/admin/api_detaills/GlobalStates/WinningDiceBetListSlice";
+
+
 const store = configureStore({
   reducer: {
     diceSummary: diceSummaryReducer,
     NigerianUsers: NigerianUsersReducer,
     OnlineUser: OnlineUsersReducers,
     DiceBetsList: DiceBetsListReducer,
+    WinningDiceBetsList: WinningDiceBetsListReducer,
+    LoosingDiceBetsList: LoosingDiceBetsListReducer,
 
   },
 });
